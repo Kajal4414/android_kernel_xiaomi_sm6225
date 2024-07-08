32315a8d6064aad0f95e0f0ca306f0629dfa57a3
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-08 20:52:25 +0530

    Update gitpod.sh

--- gitpod.sh
+++ gitpod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-sudo apt install -y elfutils libarchive-tools flex bc
+sudo apt install -y elfutils libarchive-tools flex bc cpio
 
 # Clone Clang compiler if not already cloned
 if [ ! -d "clang-r450784e" ]; then
@@ -34,13 +34,13 @@ make O=out ARCH=arm64 vendor/spes-perf_defconfig \
     CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm \
     OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    LLVM=1 LLVM_IAS=1 Image.gz dtbo.img -j$(nproc --all) 2>&1 | tee build.log
+    LLVM=1 LLVM_IAS=1 Image.gz dtbo.img -j$(nproc --all) 2> error.log
 
-# Display last 50 lines of log if build.log exists
-if [ -f "build.log" ]; then
+# Display last 50 lines of log if error.log exists
+if [ -s "error.log" ]; then
     clear
     echo -e "\e[1;31mBuild failed. Please check the error logs below:\e[0m"
-    tail -n 50 build.log
+    tail -n 50 error.log
 fi
 
 # Package kernel into zip if build successful
