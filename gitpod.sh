951e755f961a7297433e4817f1c1bd38a8534c06
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-08 17:35:22 +0530

    Create gitpod.sh

---
+++ gitpod.sh
@@ -0,0 +1,9 @@
+sudo apt install -y elfutils libarchive-tools flex bc
+
+git clone --depth 1 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone.git clang-r450784e
+
+export KBUILD_BUILD_USER=nobody
+export KBUILD_BUILD_HOST=android-build
+export PATH="$(pwd)/clang-r450784e/bin:$PATH"
+
+rm -rf out && make O=out ARCH=arm64 vendor/spes-perf_defconfig CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 Image.gz dtbo.img -j$(nproc --all)
